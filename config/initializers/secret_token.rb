8d4fc4bb852e676a7022c3f110cd71c509406388
Akira Ogiso <ogiso.a@5sg.jp>
2013-12-06 11:40:41 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuzzleFifteen::Application.config.secret_token = '9398bae98d4914d17478ffb9a9737bcae5b173efa7cb43c6ad03f01882552a384a075930a5a807f913917e47212fcf76d22d5dbd5eae275b1852ca6c3b7de5b5'
